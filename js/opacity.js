9bd9d739be9de7692337a2bceb2c72906ee09d76
Jason111nn <jason5j288@gmail.com>
2024-11-07 12:30:09 +0800

    opacity

--- js/opacity.js
+++ js/opacity.js
@@ -1,5 +1,14 @@
 /* opacity */
 
-function OpacityUp(UP) {
-
+function OpacityUp(up) {
+    document.querySelectorAll(up).forEach((element)=> {
+        let currentOpacity = parseFloat(getComputedStyle(element).opacity);
+        element.style.opacity = (currentOpacity + 0.2).toString();
+    });
+}
+function OpacityDown(down) {
+    document.querySelectorAll(down).forEach((element)=> {
+        let currentOpacity = parseFloat(getComputedStyle(element).opacity);
+        element.style.opacity = (currentOpacity + 0.2).toString();
+    });
 }
